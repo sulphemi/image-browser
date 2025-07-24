b70d8702ca57bf63ab164b4b958b4baf2dcf39c7
Jing Qian <jqian2272@gmail.com>
2025-07-24 17:35:52 -0400

    styling imagepage wip

--- src/pages/ImagePage.tsx
+++ src/pages/ImagePage.tsx
@@ -14,9 +14,16 @@ function ImagePage() {
   }, [ sanitizedFilename ])
 
   return (
-    <div>
-      <img src={`/api/files/${sanitizedFilename}`} alt={filename} />
-      <p>{JSON.stringify(metadata)}</p>
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex w-[100%] h-screen">
+        <div className="bg-black w-[80%]">
+          <img src={`/api/files/${sanitizedFilename}`} alt={filename}
+            className="mx-auto"/>
+        </div>
+        <div className="rounded-m p-8 bg-white w-[20%] break-words min-w-100">
+          <p className="">{JSON.stringify(metadata)}</p>
+        </div>
+      </div>
     </div>
   )
 }
