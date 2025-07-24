9649849c2f2f454a165766d461b09fb476a73339
Jing Qian <jqian2272@gmail.com>
2025-07-24 17:44:57 -0400

    make cards clickable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,12 @@
 function Card({ file }) {
   return (
-    <div className="overflow-hidden rounded-xl shadow-md bg-white p-2 cursor-pointer">
-      <img src={`/api/files/${encodeURIComponent(file)}`} alt={file}
-        className="w-full h-80 object-cover rounded-lg transition-transform duration-200 hover:brightness-110"
-      />
-    </div>
+    <a href={`./image/${file}`}>
+      <div className="overflow-hidden rounded-xl shadow-md bg-white p-2 cursor-pointer">
+        <img src={`/api/files/${encodeURIComponent(file)}`} alt={file}
+          className="w-full h-80 object-cover rounded-lg transition-transform duration-200 hover:brightness-110"
+        />
+      </div>
+    </a>
   );
 }
 
