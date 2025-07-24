eb5d36ac9184611fb3f4c3f98c643f86dcbfc2bf
Jing Qian <jqian2272@gmail.com>
2025-07-24 16:47:23 -0400

    api call to metadata

--- src/pages/ImagePage.tsx
+++ src/pages/ImagePage.tsx
@@ -1,11 +1,22 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function ImagePage() {
   const { filename } = useParams()
+  const [ metadata, setMetadata ] = useState(null);
+  const sanitizedFilename = encodeURIComponent(filename);
+
+  useEffect(() => {
+    fetch(`/api/files/${sanitizedFilename}/metadata`)
+      .then(res => res.json())
+      .then(data => setMetadata(data))
+      .catch(err => console.error("Error: fetch failed", err))
+  }, [ sanitizedFilename ])
 
   return (
     <div>
-      <img src={`/api/files/${encodeURIComponent(filename)}`} alt={filename} />
+      <img src={`/api/files/${sanitizedFilename}`} alt={filename} />
+      <p>{JSON.stringify(metadata)}</p>
     </div>
   )
 }
