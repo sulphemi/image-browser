7ab5712713f1e9e5fcd05f041df1e24176115dc8
Jing Qian <jqian2272@gmail.com>
2025-07-31 03:48:43 -0400

    keep track of current index when going into imagepage

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
 
 interface CardProps {
+  index: number
   file: string
 }
 
-function Card({ file }: CardProps) {
+function Card({ index, file }: CardProps) {
   return (
-    <Link to={`./image/${file}`}>
+    <Link to={`./image/${file}`}
+      onClick={ () => {localStorage.setItem("imageIndex", index.toString())}}
+    >
       <div className="rounded-xl shadow-md bg-white p-2 cursor-pointer hover:scale-105 transition-transform duration-200">
         <div className="w-full aspect-[1/1] overflow-hidden">
           <img src={`/api/files/${encodeURIComponent(file)}/thumb`} alt={file}
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -31,7 +31,7 @@ function Gallery() {
       <h1 className="text-2xl font-bold mb-4">bleh bleh bleh bleh</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {currentFiles.map((file, idx) => (
-          <Card key={idx} file={file} />
+          <Card key={idx} file={file} index={itemsPerPage * (currentPage - 1) + idx} />
         ))}
       </div>
 
